3613a9fa9bdb8661e6153e64f4039fdc5a699568
wenming <wenming@xiaoheiban.cn>
2019-11-03 17:52:57 +0800

    Initial Commit

---
+++ tutorial/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tutorial
+//
+//  Created by Statumer Wen on 2019/11/3.
+//  Copyright © 2019 Statumer Wen. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
