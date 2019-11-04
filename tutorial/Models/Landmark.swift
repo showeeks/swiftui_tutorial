b51cdcb3eba715629cffabb4c652ead03bb15728
pdeantihuman <pdeantihuman@gmail.com>
2019-11-04 16:40:15 +0800

    add assets create data.swift LandMark.swift

---
+++ tutorial/Models/Landmark.swift
@@ -0,0 +1,44 @@
+//
+//  Landmark.swift
+//  tutorial
+//
+//  Created by Statumer Wen on 2019/11/4.
+//  Copyright © 2019 Statumer Wen. All rights reserved.
+//
+
+import SwiftUI
+import CoreLocation
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    fileprivate var imageName: String
+    fileprivate var coordinates: Coordinates
+    var state: String
+    var park: String
+    var category: Category
+    
+    var locationCoordinate: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude, longitude: coordinates.longtitude
+        )
+    }
+    
+    enum Category: String, CaseIterable, Codable, Hashable {
+        case featured = "Featured"
+        case lakes = "Lakes"
+        case rivers = "Rivers"
+    }
+    
+}
+
+extension Landmark {
+    var image: Image {
+        ImageStore.shared.image(name: imageName)
+    }
+}
+
+struct Coordinates: Hashable, Codable {
+    var latitude: Double
+    var longtitude: Double
+}
